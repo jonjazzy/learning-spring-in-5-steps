7c225313fffe5a9205b5f7267341710606bf1a28
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-06 19:52:41 -0600

    FIrst commit

---
+++ src/main/java/com/learning/spring/basics/learning/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,10 @@
+package com.learning.spring.basics.learning.springin5steps;
+
+public class BinarySearchImpl
+{
+    //  (1) Sort the Array
+
+    //  (2) Search the Array
+
+    //  (3) Return the result [index]
+}
