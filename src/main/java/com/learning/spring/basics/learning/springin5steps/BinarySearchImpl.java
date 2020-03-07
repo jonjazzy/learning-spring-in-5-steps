b02e0623ef5f49b05986ed8962a6dbee0006f439
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-06 20:13:14 -0600

    IMplementing Binary Search MEthod

--- src/main/java/com/learning/spring/basics/learning/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/learning/spring/basics/learning/springin5steps/BinarySearchImpl.java
@@ -2,9 +2,21 @@ package com.learning.spring.basics.learning.springin5steps;
 
 public class BinarySearchImpl
 {
-    //  (1) Sort the Array
+    public int binarySearch(int[] numbers, int numberToSearchFor)
+    {
+        //  (1) Sort the Array
+        if(numbers.length > 0)
+        {
+            //DO something
+        }
 
-    //  (2) Search the Array
+        //  (2) Search the Array
+        for(int i = 0; i < numbers.length; i++)
+        {
+            //DO something else
+        }
 
-    //  (3) Return the result [index]
+        //  (3) Return the result [index]
+        return 3;
+    }
 }
